9b7255ddb2dd37b6da829741c06f8b9d32391b7a
EliTheSurfer <elimane.sall@telecomnancy.net>
2017-08-07 04:50:12 +0000

    Implementation de tests pour un pattern builder de comptes banquaires

---
+++ src/test/java/model/account/accountBuilderTest.java
@@ -0,0 +1,32 @@
+package model.account;
+
+import org.junit.Test;
+
+public class accountBuilderTest {
+
+	@Test
+	public void testCreateAccount() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testWhichIdIs() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testWhichBelongsTo() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testWithBalance() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testBuild() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
